da0f75e49708842d24921334632a0020653500ef
awe19001 <joeverlyncapute@gmail.com>
2020-10-02 16:31:02 -0600

    em

--- week3/template/quizman.js
+++ week3/template/quizman.js
@@ -1,41 +1,45 @@
 const quiz = [
-  { name: "Superman",realName: "Clark Kent" },
-  { name: "Wonder Woman",realName: "Diana Prince" },
-  { name: "Batman",realName: "Bruce Wayne" },
-  ];
+  ["What is Superman's real name?","Clark Kent"],
+  ["What is Wonder Woman's real name?","Dianna Prince"],
+  ["What is Batman's real name?","Bruce Wayne"]
+];
+const view = {
+  score: document.querySelector('#score strong'),
+  question: document.getElementById('question'),
+  result: document.getElementById('result'),
+  info: document.getElementById('info'),
+  render(target,content,attributes) {
+  for(const key in attributes) {
+  target.setAttribute(key, attributes[key]);
+  }
+  target.innerHTML = content;
+  }
+  };
   
-  const game = {
-    start(quiz){
-    this.questions = [...quiz];
-    this.score = 0;
-    // main game loop
-    for(const question of this.questions){
-    this.question = question;
-    this.ask();
-    }
-    // end of main game loop
-    this.gameOver();
-    },
-    ask(){
-    const question = `What is ${this.question.name}'s real
-    ➥ name?`;
-    const response = prompt(question);
-    this.check(response);
-    },
-    check(response){
-    const answer = this.question.realName;
-    if(response === answer){
-    alert('Correct!');
-    this.score++;
-    } else {
-    alert(`Wrong! The correct answer was ${answer}`);
-    }
-    },
-    gameOver(){
-    alert(`Game Over, you scored ${this.score} point${this.score
-    !== 1 ? 's' : ''}`);
-    }
-    }
   
-
-
+function start(quiz){
+  let score = 0;
+  // main game loop
+  for(const [question,answer] of quiz){
+      const response = ask(question);
+      check(response,answer);
+  }
+  // end of main game loop
+  gameOver();
+  // function declarations
+  function ask(question){
+      return prompt(question);
+  }
+  function check(response,answer){
+      if(response === answer){
+      alert('Correct!');
+      score++;
+      } else {
+      alert(`Wrong! The correct answer was ${answer}`);
+      }
+  }
+  function gameOver(){
+      alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+  }
+}
+start(quiz);
