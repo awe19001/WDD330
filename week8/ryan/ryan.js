a7a7e2009e10e7a9c64aceb712ed384ebedb9220
awe19001 <joeverlyncapute@gmail.com>
2020-11-06 16:05:13 -0700

    star swapi

--- week8/ryan/ryan.js
+++ week8/ryan/ryan.js
@@ -1,4 +1,4 @@
- // Model
+  // Model
             // Return Planet Data as JSON Object
             function getPlanets(url){
                 return fetch(url).then((response) =>{
@@ -14,13 +14,11 @@
                     console.log(error);
                 })
             }
-​
         // View
             // Create Table Elements that will hold Planet Contents
             function renderPlanets(planetData, planetElement){
                 const list = planetElement.children[1];
                 list.innerHTML = "";
-​
                 // Loop through all the Planets
                 planetData.forEach((planet) =>{
                     let row = document.createElement("tr");
@@ -29,27 +27,22 @@
                         <td>${planet.climate}</td>
                         <td>${planet.population}</td>
                     `;
-​
                     row.addEventListener("click", (event) =>{
                         event.preventDefault(); 
                         // Function that provides details on planet 
                         getPlanetDetails(planet.url); 
                     });
-​
                     list.appendChild(row); 
                 });
             }
-​
         // Controller
             // Make Next and Previous Buttons Scroll Through List
             function showPlanets(url = "https://swapi.dev/api/planets/"){
                 getPlanets(url).then((data) =>{
                     const results = data.results; 
-​
                     const planetElement = document.getElementById("planetList");
                     // Function that will will create table elements and fill contents with planet data
                     renderPlanets(results, planetElement);
-​
                     if(data.next){
                         const next = document.getElementById("next");
                         next.onclick = () => {
@@ -64,12 +57,10 @@
                     }
                 });
             }
-​
             function getPlanetDetails(url){
                 getPlanets(url).then((data)=>{
                     console.log(planetData); 
                 });
             }
-​
         getPlanets("https://swapi.dev/api/planets/");
         showPlanets();  
