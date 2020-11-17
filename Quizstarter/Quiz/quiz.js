2663fbf4a31253c4882da6534c2fed20d573b5cf
awe19001 <59641535+awe19001@users.noreply.github.com>
2020-11-16 18:09:19 -0700

    images

--- Quizstarter/Quiz/quiz.js
+++ Quizstarter/Quiz/quiz.js
@@ -35,6 +35,57 @@ let questions = [
         choiceC : "Correct",
         correct : "C"
     }
+    {
+        question : "What does HTML stand for?",
+        imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    }
+    {
+        question : "What does HTML stand for?",
+        imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    }
 ];
 
 // create some variables
