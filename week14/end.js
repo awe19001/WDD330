4dcc2acab438601ebd3e739ae4035c0cf9140d46
awe19001 <59641535+awe19001@users.noreply.github.com>
2020-12-11 20:26:38 -0700

    hgh

--- week14/end.js
+++ week14/end.js
@@ -4,9 +4,9 @@ const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
+ 
 const MAX_HIGH_SCORES = 5;
-
+console.log(highScores);
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -18,13 +18,15 @@ saveHighScore = e => {
     e.preventDefault();
 
     const score = {
-        score: mostRecentScore,
+        score: Math.floor(Math.random()*100),
         name: username.value,
     };
     highScores.push(score);
     highScores.sort((a, b) => b.score - a.score);
     highScores.splice(5);
 
+    console.log(highScores);
+
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('/index.html');
 };
